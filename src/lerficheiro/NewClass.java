208eb2ae8a6300b285c9b34c27bee65fe852679d
csampedroromero <csampedroromero@victoria25.danielcastelao.org>
2018-02-16 14:13:56 +0100

    Cambio.

---
+++ src/lerficheiro/NewClass.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lerficheiro;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author csampedroromero
+ */
+//public class NewClass {
+//    Scanner sc;
+//File fich;
+//int suma=0,num;
+//public void suma(){
+//    fich=new File("/home/local/DANIELCASTELAO/csampedroromero/Documentos/Numero.txt");
+//
+//     try{
+//            sc= new Scanner(fich).useDelimiter("\\s*,\\s*"); 
+//            while(sc.hasNextInt()){
+//            num= sc.nextInt();
+//
+//            if (num2==0){
+//                suma+=num;
+//                
+//            }
+//        }catch (FileNotFoundException ex){
+//         System.out.println("error"+ex.getMessage());
+//        }
+//        finally{
+//            sc.close();
+//        }
+//}
+//}
