66d24b8c0ac3c170fac65d491ecb4529818151b7
csampedroromero <csampedroromero@victoria25.danielcastelao.org>
2018-02-16 14:15:14 +0100

    Cambio.

---
+++ src/persoas/Alumnos.java
@@ -0,0 +1,41 @@
+
+package persoas;
+
+public class Alumnos {
+   
+    String nome;
+    int nota;
+
+    public Alumnos() {
+    }
+
+    public Alumnos(String nome, int nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumnos{" + "nome=" + nome + ", nota=" + nota + '}';
+    }
+    
+    
+     
+    
+}
